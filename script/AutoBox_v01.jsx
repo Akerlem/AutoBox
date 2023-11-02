41ba4e39b8b8f5ddf8cab16bc2cde789b1e1a6c6
Akerlem <111512662+Akerlem@users.noreply.github.com>
2023-11-02 20:21:20 +0200

    Initial

---
+++ script/AutoBox_v01.jsx
@@ -0,0 +1,95 @@
+var window = new Window("palette","",undefined);
+window.orientation = "column";
+var groupThree = window.add("group", undefined, "groupThree");
+groupThree.orientation = "row";
+var applyButton = groupThree.add("button", undefined, "Create Box");
+
+window.center();
+window.show();
+
+applyButton.onClick = function()
+{
+    
+    if(app.project.activeItem == null){
+        alert ("Select a comp")
+        return false;
+    }
+
+    var comp = app.project.activeItem;
+//test
+    var proj = app.project;
+    var theComp = proj.activeItem;
+    var selectedLayers = theComp.selectedLayers;
+    
+    if(selectedLayers.length < 1){
+        alert("Select a text layer");
+        return false;
+    }
+    var checkedTextLayers = checkForTextLayers(selectedLayers);
+    if(checkedTextLayers.length < 1){
+        alert("Select at least 1 text layer");
+        return false;
+    }
+    //alert(checkedTextLayers);
+
+    
+    
+    app.beginUndoGroup("Create Shape Layer");
+    
+     
+    var boxControls;
+
+    // Create a new shape layer
+    var shapeLayer = comp.layers.addShape();
+    
+    shapeLayer.name = "Text Box";
+    
+    // Add a rectangle shape to the shape layer
+  
+    var shapeGroup = shapeLayer.property("Contents").addProperty("ADBE Vector Group");
+
+    var pathGroup = shapeLayer.property("Contents").property("Group 1").property("Contents").addProperty("ADBE Vector Shape - Rect");
+     //Add ex[ressions
+    var codeSize = 's=thisComp.layer(index-1);\rw=s.sourceRectAtTime().width;\rh=s.sourceRectAtTime().height;\roffset=effect("Offset")("Slider");\r[w+offset,h+offset]';
+    pathGroup.property("Size").expression = codeSize;
+    var codePosition = 's=thisComp.layer(index-1);\rw=s.sourceRectAtTime().width/2;\rh=s.sourceRectAtTime().height/2;\rl=s.sourceRectAtTime().left;\rt=s.sourceRectAtTime().top;\r[w+l,h+t]';
+    pathGroup.property("Position").expression = codePosition;
+
+    var fillGroup = shapeLayer.property("Contents").property("Group 1").property("Contents").addProperty("ADBE Vector Graphic - Fill");
+
+    shapeLayer.property("ADBE Transform Group").property("ADBE Position").expression = 'thisComp.layer(index-1).position';
+    shapeLayer.property("ADBE Transform Group").property("ADBE Anchor Point").expression = 'a = thisLayer.sourceRectAtTime();\rheight = a.height;\rwidth = a.width;\rtop = a.top;\rleft = a.left;\rx = left + width/2;\ry = top + height/2;\r[x,y];';
+    //
+
+
+    //Chnage the index of the shape layer so it will go bellow Text Layer
+    var maxIndex = -1;
+    for (var i = 0; i < selectedLayers.length; i++)
+        {
+            maxIndex = Math.max(maxIndex,selectedLayers[i].index);
+        }
+    var lastSelectedLayer = theComp.layer(maxIndex);
+    
+    shapeLayer.moveAfter(lastSelectedLayer);
+    //Center anchor point for text layer
+    lastSelectedLayer.property("ADBE Transform Group").property("ADBE Anchor Point").expression = 'a = thisLayer.sourceRectAtTime();\rheight = a.height;\rwidth = a.width;\rtop = a.top;\rleft = a.left;\rx = left + width/2;\ry = top + height/2;\r[x,y];';
+    
+    boxControls = shapeLayer.Effects.addProperty("ADBE Slider Control");
+    boxControls.name = "Offset";
+    
+    app.endUndoGroup();
+    
+
+
+}
+
+function checkForTextLayers(layers){
+    var textLayers = [];
+    for(var i=0;i < layers.length; i++){
+        if(layers[i].property("Source Text")){
+            textLayers.push(layers[i]);
+        }
+    }
+    return textLayers;
+    
+}
